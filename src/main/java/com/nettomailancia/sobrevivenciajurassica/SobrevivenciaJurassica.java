4e6d7a3762d2febe9f077c9f431637829817f516
edvardo1 <eduardobeloni@mailfence.com>
2026-06-24 18:43:30 -0300

    first commit

---
+++ src/main/java/com/nettomailancia/sobrevivenciajurassica/SobrevivenciaJurassica.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nettomailancia.sobrevivenciajurassica;
+
+/**
+ * @author ebm
+ */
+public class SobrevivenciaJurassica {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
