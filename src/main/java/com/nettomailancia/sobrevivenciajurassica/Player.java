5005da3ed315b496ebd756ba42f2cb46430a9112
edvardo1 <eduardobeloni@mailfence.com>
2026-06-29 09:43:23 -0300

    add util methods to player

--- src/main/java/com/nettomailancia/sobrevivenciajurassica/Player.java
+++ src/main/java/com/nettomailancia/sobrevivenciajurassica/Player.java
@@ -3,105 +3,122 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.nettomailancia.sobrevivenciajurassica;
+
 import java.util.Random;
 import java.util.Scanner;
+
 /**
  *
  * @author joaop
  */
 public class Player extends Entity {
+
     private int posX;
     private int posY;
     private int perception;
     private int medkits;
     private int darts;
-    private Boolean shockBat;
-    
-   public void Player(int X, int Y, int difficulty){
+    private boolean shockBat;
+
+    Player(int x, int y, int difficulty) {
         hp = 5;
-        posX = X;
-        posY = Y;
+        posX = x;
+        posY = y;
         perception = difficulty;
         medkits = 0;
-        darts = 0;  
+        darts = 0;
         shockBat = false;
     }
-   
+
     public char getChar() {
         return '@';
     }
 
-    public void move(){
+    public void move() {
         System.out.println("Qual posição de destino? ");
         Scanner scanner = new Scanner(System.in);
         String tile = scanner.next();
-        System.out.println("Tile escolhido"+tile);
+        System.out.println("Tile escolhido" + tile);
         //continuar pra realmente mover no mapa
     }
-    void damage(int d){
+
+    void damage(int d) {
         this.hp -= d;
     }
-    public void attack(Dinosaur enemy, int choice){
+
+    public void addDarts(int amount) {
+        darts += amount;
+    }
+
+    public void loseDarts(int amount) {
+        darts -= amount;
+    }
+
+    public void acquireShock() {
+        shockBat = true;
+    }
+
+    public void attack(Dinosaur enemy, int choice) {
         Random d6 = new Random();
         int result = d6.nextInt(6);
-        
-        switch(choice){
+
+        switch (choice) {
             case 1:
                 if (enemy instanceof TRex) {
                     System.out.println("T-rex é imune a mão");
-                }else{
+                } else {
                     System.out.println("Atacando com a mão");
-                    if(result == 6){
+                    if (result == 6) {
                         enemy.damage(2);
                         System.out.println("Acerto critico!");
-                    }else if(result < 3){
+                    } else if (result < 3) {
                         System.out.println("Erro crítico!");
-                    }else{
+                    } else {
                         System.out.println("Acerto");
                         enemy.damage(1);
                     }
                 }
-            break;
+                break;
             case 2:
-                if(shockBat){
+                if (shockBat) {
                     System.out.println("Atacando com o bastão");
-                    if(result >= 5){
+                    if (result >= 5) {
                         enemy.damage(2);
                         System.out.println("Acerto critico!");
-                    }else if(result == 1){
+                    } else if (result == 1) {
                         System.out.println("Erro crítico!");
-                    }else{
+                    } else {
                         System.out.println("Acerto");
                         enemy.damage(1);
                     }
                 }
-            break;
+                break;
             case 3:
-                if(darts > 0){
+                if (darts > 0) {
                     System.out.println("Atacando com a arma de dardos");
                     darts--;
-                    if(enemy instanceof Velociraptor){
+                    if (enemy instanceof Velociraptor) {
                         System.out.println("Raptor desvia!");
-                    }else{
+                    } else {
                         enemy.damage(2);
                     }
                 }
-            break;
+                break;
         }
     }
-    public Boolean dodge(){
+
+    public Boolean dodge() {
         Random d3 = new Random();
         int result = d3.nextInt(3);
-        if(result <= this.perception){
+        if (result <= this.perception) {
             return true;
-        }else{
+        } else {
             damage(1);
             return false;
         }
     }
-    public void heal(){
+
+    public void heal() {
         hp = 5;
     }
-    
-  
 }
