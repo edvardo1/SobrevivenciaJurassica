dbff8bba2d065e4d3ab5e9f58d0edc209c729708
edvardo1 <eduardobeloni@mailfence.com>
2026-06-30 09:33:09 -0300

    add rng singleton

---
+++ src/main/java/com/nettomailancia/sobrevivenciajurassica/Rng.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nettomailancia.sobrevivenciajurassica;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ebm
+ */
+public class Rng {
+    private Random random;
+    private static Rng instance;
+    
+    private Rng() {
+        random = new Random();
+    }
+    
+    public static Rng getInstance() {
+        if (instance == null) {
+            instance = new Rng();
+        }
+        return instance;
+    }
+    
+    public int dice(int sides) {
+        return 1 + random.nextInt(sides);
+    }
+}
