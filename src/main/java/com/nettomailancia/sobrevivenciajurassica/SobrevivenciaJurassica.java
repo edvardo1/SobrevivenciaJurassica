94b17f4f83b4a36a3667088564e60a9110c53242
edvardo1 <eduardobeloni@mailfence.com>
2026-06-30 19:25:11 -0300

    add title screen

--- src/main/java/com/nettomailancia/sobrevivenciajurassica/SobrevivenciaJurassica.java
+++ src/main/java/com/nettomailancia/sobrevivenciajurassica/SobrevivenciaJurassica.java
@@ -3,14 +3,29 @@
  */
 package com.nettomailancia.sobrevivenciajurassica;
 
+import java.util.Scanner;
+
 /**
  * @author ebm
  */
 public class SobrevivenciaJurassica {
+
     public static void main(String[] args) {
-        Game game = new Game();
-        while (!game.quit()) {
-            game.run();
+        while (true) {
+            System.out.println("S O B R E V I V E N C I A");
+            System.out.println("    J U R A S S I C A");
+            System.out.println("1. Jogar");
+            System.out.println("2. Sair");
+            Scanner terminalInput = new Scanner(System.in);
+            String s = terminalInput.nextLine();
+            if ("1".equals(s)) {
+                Game game = new Game();
+                while (!game.quit()) {
+                    game.run();
+                }
+            } else if ("2".equals(s)) {
+                break;
+            }
         }
     }
 }
