21e8ad754b65b365473ca83aa95cebc4eeb358a5
edvardo1 <eduardobeloni@mailfence.com>
2026-06-30 19:26:23 -0300

    remove stupid ass comments

--- src/main/java/com/nettomailancia/sobrevivenciajurassica/Troodon.java
+++ src/main/java/com/nettomailancia/sobrevivenciajurassica/Troodon.java
@@ -22,18 +22,14 @@ public class Troodon extends Dinosaur {
         if (Math.abs(distance_x) >= Math.abs(distance_y)) {
             if (distance_x > 0) {
                 d = Direction.EAST;
-                System.out.println("moving east");
             } else {
                 d = Direction.WEST;
-                System.out.println("moving west");
             }
         } else {
             if (distance_y > 0) {
                 d = Direction.SOUTH;
-                System.out.println("moving south");
             } else {
                 d = Direction.NORTH;
-                System.out.println("moving north");
             }
         }
         return tryMoveDir(game, p, tm, d);
