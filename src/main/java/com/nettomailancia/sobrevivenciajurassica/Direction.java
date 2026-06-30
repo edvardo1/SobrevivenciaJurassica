a6fc866c7013a0984209aebfbe0becc963e2a50e
edvardo1 <eduardobeloni@mailfence.com>
2026-06-30 08:34:46 -0300

    add Direction

---
+++ src/main/java/com/nettomailancia/sobrevivenciajurassica/Direction.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.nettomailancia.sobrevivenciajurassica;
+
+/**
+ *
+ * @author ebm
+ */
+public enum Direction {
+    NORTH, SOUTH, EAST, WEST;
+}
