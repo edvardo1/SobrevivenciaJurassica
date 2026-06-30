30972fb6ccbc4dffb2ad396aac75f0baddc12991
edvardo1 <eduardobeloni@mailfence.com>
2026-06-30 14:05:37 -0300

    separate turns in Battle

--- src/main/java/com/nettomailancia/sobrevivenciajurassica/Battle.java
+++ src/main/java/com/nettomailancia/sobrevivenciajurassica/Battle.java
@@ -3,34 +3,91 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.nettomailancia.sobrevivenciajurassica;
+
 import java.util.Scanner;
+
 /**
  *
  * @author joaop
  */
 public class Battle {
-    private Player number1;
+
+    private Player player;
     private Dinosaur foe;
-    Scanner scanner = new Scanner(System.in);
-    
-     
-    public void Battle (Player n1, Dinosaur f){
-        number1 = n1;
+    boolean isAmbush;
+
+    public Battle(Player p, Dinosaur f, boolean ambush) {
+        player = p;
         foe = f;
+        isAmbush = ambush;
     }
-    public Player getPlayer(){
-        return number1;
+
+    private void playerTurn() {
+        Scanner terminalInput = new Scanner(System.in);
+        boolean validOption = true;
+        do {
+            validOption = true;
+            System.out.println("Opcoes:");
+            System.out.println("r. Fugir");
+            System.out.println("p. Lutar com punhos");
+            if (player.hasDarts()) {
+                System.out.println("d. Atirar dardo");
+            }
+            String s = terminalInput.nextLine();
+
+            if (s.equals("r")) {
+                /* run away */
+            } else if (s.equals("p")) {
+                if (player.hasShock()) {
+                    getFoe().damageShockBaton();
+                } else {
+                    getFoe().damageHand();
+                }
+                System.out.println("Voce atacou o " + getFoe().getName() + "!");
+                System.out.println("foehp: " + getFoe().getHp());
+            } else if (player.hasDarts() && s.equals("d")) {
+                getFoe().damageDart();
+                player.loseDarts(1);
+            } else {
+                validOption = false;
+                System.out.println("Opcao invalida");
+            }
+        } while (!validOption);
     }
-    public Dinosaur getDinosaur(){
-        return foe;
-    }
-    public void runBattle(){
-        System.out.println("Atacar com:\n 1. A mão\n 2. Bastão\n 3. Arma de dardos");
-        int choice = scanner.nextInt();
-        number1.attack(foe, choice);
-        if(foe.getHp() > 0){
-            number1.dodge();
+
+    private void foeTurn() {
+        if (getFoe().getHp() > 0) {
+            if (Rng.getInstance().dice(3) <= player.getPerception()) {
+                System.out.println("O " + getFoe().getName() + " te ataca!");
+                getFoe().attackPlayer(player);
+            }
         }
     }
-}
 
+    public void turn() {
+        System.out.println("battle turn");
+        if (isAmbush) {
+            System.out.println("AMBUSH!");
+            foeTurn();
+            isAmbush = false;
+        } else {
+            playerTurn();
+            foeTurn();
+        }
+        if (getFoe().getHp() <= 0) {
+            System.out.println("O " + getFoe().getName() + " esta morto.");
+        }
+    }
+
+    public boolean isOver() {
+        return player.getHp() <= 0 || getFoe().getHp() <= 0;
+    }
+
+    public Dinosaur getFoe() {
+        return foe;
+    }
+
+    public void setFoe(Dinosaur foe) {
+        this.foe = foe;
+    }
+}
